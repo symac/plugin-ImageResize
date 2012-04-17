38af28533815445bc9e09ef6b4a10d33131917fb
symac <wikitipiac@laposte.net>
2012-04-17 17:59:50 +0300

    Adaptation pour besoin local

--- models/ImageResizeProcess.php
+++ models/ImageResizeProcess.php
@@ -7,6 +7,13 @@ class ImageResizeProcess extends ProcessAbstract
     
     public function run($constraints)
     {
+        // Nouvelle variable qui permet de ne pas reconstruire les images si elles existent 
+        // déjà. À mettre à true si on veut utiliser le plugin selon sa fonction originale,
+        // à savoir pour reconstruire toutes les images. L'usage avec false permet de charger 
+        // des lots de fichiers (via Dropbox) sans générer les vignettes à la volée, et sans pour
+        // autant être obligé de regénérer toutes les vignettes à chaque fois, mais simplement le diff
+        $SMA_REBUILD = false;
+        
         $db = get_db();
         $storage = Zend_Registry::get('storage');
         
@@ -39,29 +46,57 @@ class ImageResizeProcess extends ProcessAbstract
                     continue;
                 }
                 
+                // On va tester si le fichier existe déjà
+                $sma_src = ARCHIVE_DIR;
                 switch ($derivativeType) {
                     // Resize square thumbnail.
                     case 'square_thumbnail_constraint':
-                        $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'square_thumbnail');
-                        $source = FILES_DIR . '/square_thumbnail_' . $newFileName;
-                        $dest = $storage->getPathByType($newFileName, 'square_thumbnails');
+                        $sma_src .= $storage->getPathByType($imageFile['archive_filename'], 'square_thumbnails');
                         break;
                     // Resize thumbnail.
                     case 'thumbnail_constraint':
-                        $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'thumbnail');
-                        $source = FILES_DIR . '/thumbnail_' . $newFileName;
-                        $dest = $storage->getPathByType($newFileName, 'thumbnails');
+                        $sma_src .= $storage->getPathByType($imageFile['archive_filename'], 'thumbnails');
                         break;
                     // Resize fullsize.
                     default:
-                        $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'fullsize');
-                        $source = FILES_DIR . '/fullsize_' . $newFileName;
-                        $dest = $storage->getPathByType($newFileName, 'fullsize');
+                        $sma_src .= $storage->getPathByType($imageFile['archive_filename'], 'fullsize');
                         break;
                 }
                 
-                // Store the file.
-                $storage->store($source, $dest);
+                
+                // On va générer les vignettes si l'une des deux conditions est réunie : 
+                // * la vignette destination n'existe pas
+                // * la variable $SMA_REBUILD est à true, on veut donc tout reconstruire.
+                if ( (!file_exists($sma_src)) || ($SMA_REBUILD) )
+                {
+                    switch ($derivativeType) {
+                        // Resize square thumbnail.
+                        case 'square_thumbnail_constraint':
+                            $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'square_thumbnail');
+                            $source = FILES_DIR . '/square_thumbnail_' . $newFileName;
+                            $dest = $storage->getPathByType($newFileName, 'square_thumbnails');
+                            break;
+                        // Resize thumbnail.
+                        case 'thumbnail_constraint':
+                            $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'thumbnail');
+                            $source = FILES_DIR . '/thumbnail_' . $newFileName;
+                            $dest = $storage->getPathByType($newFileName, 'thumbnails');
+                            break;
+                        // Resize fullsize.
+                        default:
+                            $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'fullsize');
+                            $source = FILES_DIR . '/fullsize_' . $newFileName;
+                            $dest = $storage->getPathByType($newFileName, 'fullsize');
+                            break;
+                    }
+                    
+                    // Store the file.
+                    $storage->store($source, $dest);
+                }
+                else
+                {
+                    _log("Image existe déjà, pas recréée : $sma_src);
+                }
             }
         }
     }
